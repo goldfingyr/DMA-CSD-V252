bc588c81df40776d78b1e443e79df739fff4b53f
Karsten Jeppesen <karsten@jeppesens.com>
2026-03-12 12:11:29 +0100

    GrabImage, WeatherBunny: initial commit

--- GrabImage/GrabImage/Form1.Designer.cs
+++ GrabImage/GrabImage/Form1.Designer.cs
@@ -38,23 +38,26 @@
             poster_path = new ColumnHeader();
             PbSelectedImage = new PictureBox();
             BtDownload = new Button();
+            label3 = new Label();
             ((System.ComponentModel.ISupportInitialize)PbSelectedImage).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 42);
+            label1.Location = new Point(17, 70);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(27, 15);
+            label1.Size = new Size(41, 25);
             label1.TabIndex = 0;
             label1.Text = "KEY";
             // 
             // TbKey
             // 
-            TbKey.Location = new Point(12, 60);
+            TbKey.Location = new Point(17, 100);
+            TbKey.Margin = new Padding(4, 5, 4, 5);
             TbKey.Name = "TbKey";
-            TbKey.Size = new Size(279, 23);
+            TbKey.Size = new Size(397, 31);
             TbKey.TabIndex = 1;
             TbKey.Text = "Copy Key Here";
             TbKey.TextChanged += TbKey_TextChanged;
@@ -62,26 +65,29 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 109);
+            label2.Location = new Point(17, 182);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(75, 15);
+            label2.Size = new Size(114, 25);
             label2.TabIndex = 2;
             label2.Text = "Search string";
             // 
             // TbSearch
             // 
-            TbSearch.Location = new Point(12, 127);
+            TbSearch.Location = new Point(17, 212);
+            TbSearch.Margin = new Padding(4, 5, 4, 5);
             TbSearch.Name = "TbSearch";
-            TbSearch.Size = new Size(279, 23);
+            TbSearch.Size = new Size(397, 31);
             TbSearch.TabIndex = 3;
             TbSearch.Text = "What to search for";
             TbSearch.TextChanged += TbSearch_TextChanged;
             // 
             // BtSearch
             // 
-            BtSearch.Location = new Point(14, 176);
+            BtSearch.Location = new Point(20, 293);
+            BtSearch.Margin = new Padding(4, 5, 4, 5);
             BtSearch.Name = "BtSearch";
-            BtSearch.Size = new Size(75, 23);
+            BtSearch.Size = new Size(107, 38);
             BtSearch.TabIndex = 5;
             BtSearch.Text = "Search";
             BtSearch.UseVisualStyleBackColor = true;
@@ -91,9 +97,10 @@
             // 
             LWMovies.Columns.AddRange(new ColumnHeader[] { title, poster_path });
             LWMovies.FullRowSelect = true;
-            LWMovies.Location = new Point(16, 211);
+            LWMovies.Location = new Point(23, 352);
+            LWMovies.Margin = new Padding(4, 5, 4, 5);
             LWMovies.Name = "LWMovies";
-            LWMovies.Size = new Size(275, 227);
+            LWMovies.Size = new Size(391, 376);
             LWMovies.TabIndex = 6;
             LWMovies.UseCompatibleStateImageBehavior = false;
             LWMovies.View = View.Details;
@@ -110,27 +117,39 @@
             // 
             // PbSelectedImage
             // 
-            PbSelectedImage.Location = new Point(353, 60);
+            PbSelectedImage.Location = new Point(504, 100);
+            PbSelectedImage.Margin = new Padding(4, 5, 4, 5);
             PbSelectedImage.Name = "PbSelectedImage";
-            PbSelectedImage.Size = new Size(212, 367);
+            PbSelectedImage.Size = new Size(303, 612);
             PbSelectedImage.SizeMode = PictureBoxSizeMode.Zoom;
             PbSelectedImage.TabIndex = 7;
             PbSelectedImage.TabStop = false;
             // 
             // BtDownload
             // 
-            BtDownload.Location = new Point(384, 433);
+            BtDownload.Location = new Point(549, 722);
+            BtDownload.Margin = new Padding(4, 5, 4, 5);
             BtDownload.Name = "BtDownload";
-            BtDownload.Size = new Size(75, 23);
+            BtDownload.Size = new Size(107, 38);
             BtDownload.TabIndex = 8;
             BtDownload.Text = "Download";
             BtDownload.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(505, 58);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 25);
+            label3.TabIndex = 9;
+            label3.Text = "Billede";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(567, 468);
+            ClientSize = new Size(810, 780);
+            Controls.Add(label3);
             Controls.Add(BtDownload);
             Controls.Add(PbSelectedImage);
             Controls.Add(LWMovies);
@@ -139,6 +158,7 @@
             Controls.Add(label2);
             Controls.Add(TbKey);
             Controls.Add(label1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)PbSelectedImage).EndInit();
@@ -159,5 +179,6 @@
         private ColumnHeader poster_path;
         private PictureBox PbSelectedImage;
         private Button BtDownload;
+        private Label label3;
     }
 }
--- WeatherBunny/WeatherBunny/Program.cs
+++ WeatherBunny/WeatherBunny/Program.cs
@@ -13,9 +13,9 @@ var url = "https://www.timeanddate.no/vaer/?continent=europe&low=c";
 var web = new HtmlWeb();
 var dom = web.Load(url);
 var cities = dom.DocumentNode.SelectNodes("//td");
-for (int index=0; index<cities.Count; index += 4)
+for (int index = 0; index < cities.Count; index += 4)
 {
     string temperature = cities[index + 3].InnerText;
     int posAmbersand = temperature.IndexOf('&');
-    Console.WriteLine(cities[index].InnerText + "   " + cities[index+3].InnerText.Remove(posAmbersand, 6));
+    Console.WriteLine(cities[index].InnerText + "   " + cities[index + 3].InnerText.Remove(posAmbersand, 6));
 }
